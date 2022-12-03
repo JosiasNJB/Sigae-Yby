5d5cef870ebd63a2b2df853e567f5c97efac6c8e
JosiasNJB <84824433+JosiasNJB@users.noreply.github.com>
2022-12-03 20:13:54 -0300

    Update conexao_db.php

--- app/conexao_db.php
+++ app/conexao_db.php
@@ -2,21 +2,21 @@
 
 // Abre uma conexao com o BD.
 
-//$host        = "host = 127.0.0.1;";
-//$port        = "port = 6649;";
-//$dbname      = "dbname = pg_products;";
-//$dbuser 	 = "postgres";
-//$dbpassword	 = "00000000";
+$host = "host = localhost;";
+$port = "port = 330;";
+$dbname = "dbname = banco_projetointegrador;";
+$dbuser = "root";
+$dbpassword	= "usbw";
 
 // dados de conexao com o railway. Usar somente caso esteja usando railway
-$host        = "host = " . getenv("PGHOST") . ";";
-$port        = "port = " . getenv("PGPORT") . ";";
-$dbname      = "dbname = " . getenv("PGDATABASE") . ";";
-$dbuser 	 = getenv("PGUSER");
-$dbpassword	 = getenv("PGPASSWORD");
+//$host        = "host = " . getenv("PGHOST") . ";";
+//$port        = "port = " . getenv("PGPORT") . ";";
+//$dbname      = "dbname = " . getenv("PGDATABASE") . ";";
+//$dbuser 	 = getenv("PGUSER");
+//$dbpassword	 = getenv("PGPASSWORD");
 
 // para conectar ao mysql, substitua pgsql por mysql
-$db_con= new PDO('pgsql:' . $host . $port . $dbname, $dbuser, $dbpassword);
+$db_con= new PDO('mysql:' . $host . $port . $dbname, $dbuser, $dbpassword);
 
 //alguns atributos de performance.
 $db_con->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
