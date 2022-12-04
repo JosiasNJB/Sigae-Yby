f5c0ca554b0138dbb9eb3b52f209ec4d6455f022
Vitória Isabel <vitoriaisabel77351@gmail.com>
2022-12-03 22:23:34 -0300

    conexao_db

--- app/conexao_db.php
+++ app/conexao_db.php
@@ -8,13 +8,6 @@ $dbname = "dbname = banco_projetointegrador;";
 $dbuser = "root";
 $dbpassword	= "usbw";
 
-// dados de conexao com o railway. Usar somente caso esteja usando railway
-//$host        = "host = " . getenv("PGHOST") . ";";
-//$port        = "port = " . getenv("PGPORT") . ";";
-//$dbname      = "dbname = " . getenv("PGDATABASE") . ";";
-//$dbuser 	 = getenv("PGUSER");
-//$dbpassword	 = getenv("PGPASSWORD");
-
 // para conectar ao mysql, substitua pgsql por mysql
 $db_con= new PDO('mysql:' . $host . $port . $dbname, $dbuser, $dbpassword);
 
